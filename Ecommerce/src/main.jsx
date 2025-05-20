91a37baac73dbdb3351ae6515e9dacce8059363f
Ahmed Ramadan <ahmedramadan8050@gmail.com>
2025-05-20 19:28:17 +0300

    feat(cart): handle add to cart and quantites[lab3]

--- Ecommerce/src/main.jsx
+++ Ecommerce/src/main.jsx
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react'
+import { Provider } from 'react-redux';
+
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-
+import store from './store/index.js';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Provider store={store}>
     <App/>
+    </Provider>
   </StrictMode>,
 )
