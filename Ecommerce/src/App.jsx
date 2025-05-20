28143a48c479d8b1d2903c36482ea4ae281b8586
Ahmed Ramadan <ahmedramadan8050@gmail.com>
2025-05-20 22:46:51 +0300

    Apply code splitting for pages

--- Ecommerce/src/App.jsx
+++ Ecommerce/src/App.jsx
@@ -1,17 +1,17 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState ,lazy, Suspense} from 'react';
 import LanguageContext from "./context/language";
-import { BrowserRouter, Route, Routes } from 'react-router';  // نصيحة: استبدل 'react-router' بـ 'react-router-dom'
+import { BrowserRouter, Route, Routes} from 'react-router';  // نصيحة: استبدل 'react-router' بـ 'react-router-dom'
 import Header from './components/Header';
-import ProductsList from './pages/ProductsList';
-import NotFound from './pages/NotFound';
-import ProductDetails from './pages/ProductDetails';
-import Cart from './pages/Cart';
+
+const ProductsList=lazy(()=>import("./pages/ProductsList"));
+const Cart=lazy(()=>import("./pages/Cart"));
+const ProductDetails=lazy(()=>import("./pages/ProductDetails"))
+const NotFound=lazy(()=>import("./pages/NotFound"))
 
 function App() {
   const [language, setLanguage] = useState("en");
 
   useEffect(() => {
-    // تغيير اتجاه الصفحة بناءً على اللغة
     if (language === 'ar') {
       document.documentElement.dir = 'rtl';
     } else {
@@ -25,12 +25,14 @@ function App() {
         <div className='m-3'>
           <BrowserRouter>
             <Header />
+            <Suspense fallback={<h2>Loading...</h2>}>
             <Routes>
               <Route path="/" element={<ProductsList />} />
               <Route path="product-details/:id" element={<ProductDetails />} />
               <Route path="/cart" element={<Cart />} />
               <Route path="*" element={<NotFound />} />
             </Routes>
+            </Suspense>
           </BrowserRouter>
         </div>
       </div>
